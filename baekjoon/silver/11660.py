2f833d68e7f14b3e5595c913aedb401aa570da55
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-05-21 01:08:23 +0900

    구간 합 구하기

--- baekjoon/silver/11660.py
+++ baekjoon/silver/11660.py
@@ -8,11 +8,11 @@ for i in range(n):
     row = [0] + [int(x) for x in input().split()]
     A.append(row)
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
-    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+    result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1]
     print(result)
