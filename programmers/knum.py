71eeb9d470217e70f6ae68141b7314e5e93c1011
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-05-24 16:32:43 +0900

    LEVEL 1. K번째 수

--- programmers/knum.py
+++ programmers/knum.py
@@ -15,6 +15,13 @@ def solution(array, commands):
     return answer
 
 
+''' 
+2. 다른 사람 풀이 - 람다 활용 한 줄 작성
+
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+'''
+
 array = [1,5,2,6,3,7,4]
 commands = [ [2,5,3], [4,4,1], [1,7,3]]
 a = solution(array, commands)
